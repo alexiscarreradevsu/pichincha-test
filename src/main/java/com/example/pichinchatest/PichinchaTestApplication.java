71200254a00e981fb494022f5aa829c2ec770d13
Alexis Carrera <alexis.carrera@devsu.com>
2021-10-11 17:52:23 -0500

    First Commit

---
+++ src/main/java/com/example/pichinchatest/PichinchaTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.pichinchatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PichinchaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PichinchaTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pichinchatest/PichinchaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pichinchatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PichinchaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
