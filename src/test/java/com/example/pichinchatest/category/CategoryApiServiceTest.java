6eaceee05b1c433bce547d113417514db670da66
Alexis Carrera <alexis.carrera@devsu.com>
2021-10-11 23:31:21 -0500

    Add Utils in Test

--- src/test/java/com/example/pichinchatest/category/CategoryApiServiceTest.java
+++ src/test/java/com/example/pichinchatest/category/CategoryApiServiceTest.java
@@ -2,15 +2,15 @@ package com.example.pichinchatest.category;
 
 import com.example.pichinchatest.catergories.CategoriesController;
 import com.example.pichinchatest.catergories.CategoriesService;
+import com.example.pichinchatest.catergories.model.Category;
 import com.example.pichinchatest.catergories.repository.CategoriesRepository;
-import com.example.pichinchatest.catergories.representation.CreateCategoryRequest;
 import com.example.pichinchatest.test.TestDataGeneratorUtil;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 public class CategoryApiServiceTest {
 
@@ -28,10 +28,10 @@ public class CategoryApiServiceTest {
 
     @Test
     public void addCategory_shouldCallCategoryService() {
-        CreateCategoryRequest createCategoryRequest = TestDataGeneratorUtil.generateCreateCategoryRequest();
-        when(categoriesService.createNewCategory(createCategoryRequest)).thenReturn(createCategoryRequest);
-        categoriesController.addCategory(createCategoryRequest);
-        verify(categoriesService).createNewCategory(createCategoryRequest);
+        Category category = TestDataGeneratorUtil.generateCategoryCompleteModel();
+        Mockito.when(categoriesRepository.findById(1L)).thenReturn(java.util.Optional.ofNullable(category));
+        categoriesController.retrieveItemsInCategory(1L);
+        verify(categoriesController).retrieveItemsInCategory(category.getId());
     }
 
 
--- src/test/java/com/example/pichinchatest/test/TestDataGeneratorUtil.java
+++ src/test/java/com/example/pichinchatest/test/TestDataGeneratorUtil.java
@@ -1,12 +1,72 @@
 package com.example.pichinchatest.test;
 
+import com.example.pichinchatest.catergories.model.Category;
 import com.example.pichinchatest.catergories.representation.CreateCategoryRequest;
+import com.example.pichinchatest.items.model.Items;
+import com.example.pichinchatest.items.model.ItemsRecord;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public class TestDataGeneratorUtil {
 
     public static CreateCategoryRequest generateCreateCategoryRequest() {
         return CreateCategoryRequest.builder()
                 .category("category1")
+                .build();
+    }
+
+
+    public static Category generateCategoryCompleteModel() {
+        return Category.builder()
+                .id(1L)
+                .categoryName("category1")
+                .listItemsRecord( new ArrayList<ItemsRecord>() {{
+                    add(generateItemsRecordModel());
+                    add(generateItemsRecordModel2());
+                }})
+                .build();
+    }
+
+    public static Category generateCategoryModel() {
+        return Category.builder()
+                .id(1L)
+                .categoryName("category1")
+                .build();
+    }
+
+    public static ItemsRecord generateItemsRecordModel() {
+        return ItemsRecord.builder()
+                .id(1L)
+                .isIntoCategory(true)
+                .items(generateItemsModel1())
+                .category(generateCategoryModel())
+                .build();
+    }
+
+    public static ItemsRecord generateItemsRecordModel2() {
+        return ItemsRecord.builder()
+                .id(2L)
+                .isIntoCategory(true)
+                .items(generateItemsModel2())
+                .category(generateCategoryModel())
+                .build();
+    }
+
+    public static Items generateItemsModel1() {
+        return Items.builder()
+                .id(1L)
+                .name("Item1")
+                .type("Type1")
+                .build();
+    }
+
+    public static Items generateItemsModel2() {
+        return Items.builder()
+                .id(2L)
+                .name("Item2")
+                .type("Type2")
                 .build();
     }
 
